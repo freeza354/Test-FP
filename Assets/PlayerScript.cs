a03686e5b461a5ca99c1123aa54d251af3dc528b
freeza354 email = freeza354@gmail.com <freeza354@gmail.com>
2018-12-04 12:48:24 +0700

    Update Game

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float PlayerMoveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitDistance = 0f;
+
+        if(true){
+
+        }
+
+	}
+}
